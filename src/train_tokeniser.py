978e3211274ec272be67720b182de517852731cd
saattrupdan <saattrupdan@gmail.com>
2021-12-06 10:56:03 +0000

    feat: Set up unigram tokeniser

--- src/train_tokeniser.py
+++ src/train_tokeniser.py
@@ -1,11 +1,8 @@
 '''Script for training a tokeniser'''
 
 from tokenizers import (normalizers, pre_tokenizers, tokenizers,
-                        processors, trainers, models, AddedToken)
+                        processors, trainers, models, decoders, AddedToken)
 from datasets import Dataset
-import pandas as pd
-
-from compound_nouns import CompoundNounPreTokenizer
 
 
 def main():
@@ -14,8 +11,24 @@ def main():
     # Load the dataset
     dataset = Dataset.load_from_disk('data/dataset')
 
+    # Load the dictionary
+    # csv_path = 'data/retskrivningsordbog-basic.csv'
+    # dictionary = pd.read_csv(csv_path, sep=';', names=['word', 'type'])
+    # dictionary['type'] = dictionary.type.str.split(',')
+    # dictionary = (dictionary
+    #               .explode('type')
+    #               .query('type in ["sb.", "talord", "sb. pl."]')
+    #               .word
+    #               .str.replace('[0-9]+[.] ', '', regex=True)
+    #               .str.lower()
+    #               .drop_duplicates()
+    #               .sort_values(key=lambda x: x.str.len())
+    #               .tolist())
+    # dictionary = [word for word in dictionary
+    #               if ' ' not in word and len(word) > 2]
+
     # Initialise the tokeniser model
-    model = models.WordPiece(unk_token='<unk>')
+    model = models.Unigram()
 
     # Initialise the tokeniser
     tokeniser = tokenizers.Tokenizer(model=model)
@@ -26,40 +39,21 @@ def main():
         AddedToken('</s>', single_word=True, normalized=False),
         AddedToken('<unk>', single_word=True, normalized=False),
         AddedToken('<mask>', single_word=True, normalized=False),
-        AddedToken('<pad>', single_word=True, normalized=False)
+        AddedToken('<pad>', single_word=True, normalized=False),
     ]
     tokeniser.add_special_tokens(special_tokens)
 
     # Initialise the normaliser
     normaliser = normalizers.Sequence([
-        normalizers.NFKC(),
-        normalizers.Strip(),
-        normalizers.Lowercase()
+        normalizers.NFKC()
     ])
     tokeniser.normalizer = normaliser
 
-    # Load the dictionary
-    csv_path = 'data/retskrivningsordbog-basic.csv'
-    dictionary = (pd.read_csv(csv_path, sep=';', names=['word', 'type'])
-                    .query('type == "sb."')
-                    .word
-                    .str.replace('[0-9]+[.] ', '', regex=True)
-                    .str.lower()
-                    .drop_duplicates()
-                    .sort_values(key=lambda x: x.str.len())
-                    .tolist())
-    dictionary = [word for word in dictionary
-                  if ' ' not in word and len(word) > 1]
-
     # Initialise the pre-tokeniser
-    compound_noun = CompoundNounPreTokenizer(dictionary=dictionary,
-                                             interfixes=['e', 's'])
-    compound_noun_pretok = pre_tokenizers.PreTokenizer.custom(compound_noun)
     pre_tokeniser = pre_tokenizers.Sequence([
-        pre_tokenizers.Metaspace(add_prefix_space=False),
+        pre_tokenizers.Metaspace(add_prefix_space=True),
         pre_tokenizers.Punctuation(behaviour='isolated'),
-        pre_tokenizers.Digits(individual_digits=True),
-        compound_noun_pretok
+        pre_tokenizers.Digits(individual_digits=True)
     ])
     tokeniser.pre_tokenizer = pre_tokeniser
 
@@ -68,14 +62,25 @@ def main():
                                                   sep=('</s>', 1))
     tokeniser.post_processor = post_processor
 
+    # Initialise the decoder
+    decoder = decoders.Metaspace(add_prefix_space=True)
+    tokeniser.decoder = decoder
+
     # Initialise the trainer
-    trainer = trainers.WordPieceTrainer(vocab_size=50_000,
-                                        min_frequency=0,
-                                        special_tokens=special_tokens,
-                                        continuing_subword_prefix='##')
+    trainer = trainers.UnigramTrainer(vocab_size=200_000,
+                                      special_tokens=special_tokens)
 
     # Train the tokeniser
     tokeniser.train_from_iterator(iterator=dataset['text'], trainer=trainer)
+
+    # Save the tokeniser
+    tokeniser.save('dasvno-wiki.json')
+
+
+if __name__ == '__main__':
+    main()
+
+    tokeniser = tokenizers.Tokenizer.from_file('dasvno-wiki.json')
 
     # Print the vocabulary size
     print(f'Vocabulary size: {tokeniser.get_vocab_size():,}')
@@ -103,15 +108,17 @@ def main():
         'førstegenerationsindvandrer',
         'arbejdsmarkedsuddannelse',
         'halvfjerdsårsfødselsdag',
+        'rosenbusk',
+        'tornebusk',
+        'tjørnebusk',
+        'kapersbusk',
+        'ribsbusk'
     ]
     for example in test_examples:
-        print(example, compound_noun_pretok.pre_tokenize_str(example))
         print(example, tokeniser.encode(example).tokens)
         print()
 
-    # Save the tokeniser
-    # tokeniser.save('scandiwikibert')
-
-
-if __name__ == '__main__':
-    main()
+    while True:
+        input_text = input('Enter text:\n> ')
+        print(tokeniser.encode(input_text).tokens)
+        print()
