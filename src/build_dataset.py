64ae6b6eac3322a71e102485d8a8ddf516830eb2
saattrupdan <saattrupdan@gmail.com>
2021-12-06 14:08:03 +0000

    feat: Add Nynorsk wikipedia

--- src/build_dataset.py
+++ src/build_dataset.py
@@ -10,7 +10,8 @@ def main():
     da_lexdk_path = 'data/da-lexdk-preprocessed.txt'
     da_wiki_path = 'data/da-wikipedia-raw-preprocessed.txt'
     sv_wiki_path = 'data/sv-wikipedia-raw-preprocessed.txt'
-    no_wiki_path = 'data/no-wikipedia-raw-preprocessed.txt'
+    nb_wiki_path = 'data/nb-wikipedia-raw-preprocessed.txt'
+    nn_wiki_path = 'data/nn-wikipedia-raw-preprocessed.txt'
     is_wiki_path = 'data/is-wikipedia-raw-preprocessed.txt'
     fo_wiki_path = 'data/fo-wikipedia-raw-preprocessed.txt'
 
@@ -18,7 +19,8 @@ def main():
     da_dataset = concatenate_datasets([Dataset.from_text(da_lexdk_path),
                                        Dataset.from_text(da_wiki_path)])
     sv_dataset = Dataset.from_text(sv_wiki_path)
-    no_dataset = Dataset.from_text(no_wiki_path)
+    no_dataset = concatenate_datasets([Dataset.from_text(nb_wiki_path),
+                                       Dataset.from_text(nn_wiki_path)])
     is_dataset = Dataset.from_text(is_wiki_path)
     fo_dataset = Dataset.from_text(fo_wiki_path)
 
