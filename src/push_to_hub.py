a10d7730fa188ac87ddfb16c244ba76cc19d75d8
saattrupdan <saattrupdan@gmail.com>
2021-12-08 15:47:19 +0000

    fix: Also push tokenizer to hub

--- src/push_to_hub.py
+++ src/push_to_hub.py
@@ -1,6 +1,6 @@
 '''Push the model the HF Hub'''
 
-from transformers import AutoModelForPreTraining
+from transformers import AutoModelForPreTraining, AutoTokenizer
 import sys
 
 
@@ -8,6 +8,8 @@ def main():
     '''Push the model to the HF Hub'''
     if len(sys.argv) == 2:
         model_id = sys.argv[-1]
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        tokenizer.push_to_hub(model_id)
         model = AutoModelForPreTraining.from_pretrained(model_id)
         model.push_to_hub(model_id)
 
