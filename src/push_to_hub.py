a4a2fcce05881db53f3dfc38a083a97e40402eab
saattrupdan <saattrupdan@gmail.com>
2021-12-08 15:37:59 +0000

    feat: Add sys argv to push_to_hub.py

--- src/push_to_hub.py
+++ src/push_to_hub.py
@@ -1,12 +1,15 @@
 '''Push the model the HF Hub'''
 
 from transformers import AutoModelForPreTraining
+import sys
 
 
 def main():
     '''Push the model to the HF Hub'''
-    model = AutoModelForPreTraining.from_pretrained('roberta-base-wiki-da')
-    model.push_to_hub('roberta-base-wiki-da')
+    if len(sys.argv) == 2:
+        model_id = sys.argv[-1]
+        model = AutoModelForPreTraining.from_pretrained(model_id)
+        model.push_to_hub(model_id)
 
 
 if __name__ == '__main__':
